3b1a909c2091576f760ea693e39a671acd41c3d1
Ramazan Dzhekshembaev <ramazan.dzhekshembaev@jetbrains.com>
2024-08-24 17:19:27 +0200

    tg button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WebApp from "@twa-dev/sdk";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,9 +22,11 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      </div>
+      <div className="card">
+        <button onClick={() => WebApp.showAlert("Hey, wu")}>
+          Show allert button
+        </button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
