9b3608912b8f1c92106f389a9eb939455511d908
Ramazan Dzhekshembaev <ramazan.dzhekshembaev@jetbrains.com>
2024-09-08 13:48:04 +0200

    Localize shop guide to Russian and enhance UI alignment
    This commit updates the shop guide steps to Russian language and adds a thank you step. It also includes UI improvements such as centering text, adjusting element alignments, and enhancing responsiveness for better readability.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,40 +11,45 @@ import shoppingCart from './assets/shopping_cart.jpg';
 import coupon from './assets/coupon.jpg';
 import payment from './assets/payment.jpg';
 import orderConfirm from './assets/order_confirm.jpg';
+import thank_you from './assets/thank_you.jpg';
 
 // Create an array of image steps
 const steps = [
   {
     image: botCatalogButton,
-    description: 'Step 1: Click on the Catalog Button to start.'
+    description: 'Шаг 1: Нажмите на кнопку каталога, чтобы начать.'
   },
   {
     image: mainMenu,
-    description: 'Step 2: You will be taken to the Main Menu.'
+    description: 'Шаг 2: Вы будете перенаправлены в Главное меню.'
   },
   {
     image: ringsPic,
-    description: 'Step 3: Select the Rings category.'
-  },
-  {
-    image: order,
-    description: 'Step 4: Place your order by selecting an item.'
+    description: 'Шаг 3: Выберите понравившийся товар в каталоге.'
   },
   {
     image: shoppingCart,
-    description: 'Step 5: Check your Shopping Cart.'
+    description: 'Шаг 4: Перейдите в корзину покупок.'
+  },
+  {
+    image: order,
+    description: 'Шаг 5: Разместите заказ на свое имя и номер телефона. Для доставки пожалуйста введите слово "Доставка" и желаемый адрес'
   },
   {
     image: coupon,
-    description: 'Step 6: Apply a Coupon if you have one.'
+    description: 'Шаг 6: Примените купон, если он у вас есть.'
   },
   {
     image: payment,
-    description: 'Step 7: Proceed to Payment.'
+    description: 'Шаг 7: Перейдите к оплате.'
   },
   {
     image: orderConfirm,
-    description: 'Step 8: Confirm your Order.'
+    description: 'Шаг 8: Получите подтверждение заказа.'
+  },
+  {
+    image: thank_you,
+    description: 'Шаг 9: 💖Вы восхитительны!💖<br />Ожидайте ответа по доставке или забирайте заказ в нашем магазине уже сегодня!'
   }
 ];
 
@@ -52,11 +57,11 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="guide-container">
-        <h1>Shop Guide</h1>
+        <h1>Гайд по нашему магазину💖</h1>
         {steps.map((step, index) => (
           <div key={index} className="guide-step">
-            <img src={step.image} alt={`Step ${index + 1}`} />
-            <p>{step.description}</p>
+            <img src={step.image} alt={`Шаг ${index + 1}`} />
+            <p dangerouslySetInnerHTML={{ __html: step.description }} />
           </div>
         ))}
       </div>
